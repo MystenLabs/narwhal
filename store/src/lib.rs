35319b17ece0bcfc6839fb23188138424da86a27
François Garillot <francois@garillot.net>
2021-11-12 11:56:02 -0800

    chore: prepare te repo for Rust edition 2021
    Add rust_2021_compatibility lint warn to workspace members. See [doc](https://doc.rust-lang.org/rustc/lints/groups.html)

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,4 +1,11 @@
 // Copyright(C) Facebook, Inc. and its affiliates.
+#![warn(
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    rust_2021_compatibility
+)]
+
 use std::collections::{HashMap, VecDeque};
 use tokio::sync::mpsc::{channel, Sender};
 use tokio::sync::oneshot;
