c841771ca769df4dbdc7e5ec0debeb07b328756b
Anastasios Kichidis <akihidis@gmail.com>
2022-02-22 18:59:08 +0000

    [feature]: add license headers to new files

--- primary/src/collection_waiter.rs
+++ primary/src/collection_waiter.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use crate::{messages::Header, Certificate, PrimaryWorkerMessage};
 use bytes::Bytes;
 use config::Committee;
--- primary/src/tests/collection_waiter_tests.rs
+++ primary/src/tests/collection_waiter_tests.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use crate::{
     collection_waiter::{
         BatchMessage, CollectionCommand, CollectionErrorType, CollectionResult, CollectionWaiter,
