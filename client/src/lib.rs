ab726985d0935e6306939242411cc4f29f0af5de
Alberto Sonnino <alberto@sonnino.com>
2022-04-11 20:00:02 -0400

    Spawn client subscriber

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,17 +1,35 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 pub mod batch_loader;
-pub mod client;
-pub mod errors;
-pub mod utils;
+mod errors;
+pub mod subscriber;
+mod utils;
 
-use crate::errors::AuthorityStateError;
+use crate::{
+    batch_loader::BatchLoader,
+    errors::{AuthorityStateError, SubscriberResult},
+    subscriber::Subscriber,
+};
 use async_trait::async_trait;
+use config::Committee;
 use consensus::SequenceNumber;
+use crypto::traits::VerifyingKey;
+use primary::BatchDigest;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::{net::SocketAddr, sync::Arc};
+use store::{
+    reopen,
+    rocks::{open_cf, DBMap},
+    Store,
+};
+use tokio::{sync::mpsc::channel, task::JoinHandle};
+use worker::SerializedBatchMessage;
 
 /// Default inter-task channel size.
 pub const DEFAULT_CHANNEL_SIZE: usize = 1_000;
+
+/// The datastore column family names.
+const BATCHES_CF: &str = "batches";
 
 /// The state of the subscriber keeping track of the transactions that have already been
 /// executed. It ensures we do not process twice the same transaction despite crash-recovery.
@@ -88,4 +106,53 @@ pub trait AuthorityState {
 
     /// Load the last consensus index from storage.
     async fn load_subscriber_state(&self) -> Result<SubscriberState, Self::Error>;
+}
+
+/// Spawn a new client subscriber.
+pub async fn spawn_client_subscriber<ExecutionState, PublicKey>(
+    name: PublicKey,
+    committee: Committee<PublicKey>,
+    address: SocketAddr,
+    store_path: &str,
+    execution_state: Arc<ExecutionState>,
+) -> SubscriberResult<(
+    JoinHandle<SubscriberResult<()>>,
+    JoinHandle<SubscriberResult<()>>,
+)>
+where
+    ExecutionState: AuthorityState + Send + Sync + 'static,
+    PublicKey: VerifyingKey,
+{
+    let (tx_batch_loader, rx_batch_loader) = channel(DEFAULT_CHANNEL_SIZE);
+
+    // Create a temporary store to hold downloaded batches.
+    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).expect("Failed creating database");
+    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
+    let store = Store::new(batch_map);
+
+    // Spawn the subscriber.
+    let subscriber = Subscriber::<ExecutionState, PublicKey>::new(
+        address,
+        store.clone(),
+        execution_state,
+        tx_batch_loader,
+    )
+    .await?;
+    let subscriber_handle = Subscriber::spawn(subscriber);
+
+    // Spawn the batch loader.
+    let worker_addresses = committee
+        .authorities
+        .iter()
+        .find(|(x, _)| *x == &name)
+        .map(|(_, authority)| authority)
+        .expect("Our public key is not in the committee")
+        .workers
+        .iter()
+        .map(|(id, x)| (*id, x.worker_to_worker))
+        .collect();
+    let batch_loader_handle = BatchLoader::spawn(store, rx_batch_loader, worker_addresses);
+
+    // Return the handle.
+    Ok((subscriber_handle, batch_loader_handle))
 }
