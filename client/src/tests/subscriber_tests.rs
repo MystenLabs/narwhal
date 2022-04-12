80b5e013207c3489262f6a01db12a7ce0e60835d
Alberto Sonnino <alberto@sonnino.com>
2022-04-12 19:02:43 -0400

    Simplify tests

--- client/src/tests/subscriber_tests.rs
+++ client/src/tests/subscriber_tests.rs
@@ -8,12 +8,41 @@ use crate::{
 };
 use crypto::ed25519::Ed25519PublicKey;
 use primary::Certificate;
-use std::sync::Arc;
+use std::{net::SocketAddr, sync::Arc};
 use store::{
     reopen,
     rocks::{open_cf, DBMap},
 };
-use tokio::sync::mpsc::channel;
+use tokio::sync::mpsc::{channel, Sender};
+
+/// Spawn a test subscriber.
+async fn spawn_subscriber(
+    node_address: SocketAddr,
+    tx_batch_loader: Sender<ConsensusOutput<Ed25519PublicKey>>,
+) -> (
+    Store<BatchDigest, SerializedBatchMessage>,
+    Arc<TestExecutionState>,
+) {
+    // Spawn a subscriber.
+    let store_path = tempfile::tempdir().unwrap();
+    const BATCHES_CF: &str = "batches";
+    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
+    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
+    let store = Store::new(batch_map);
+
+    let execution_state = Arc::new(TestExecutionState::default());
+    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
+        node_address,
+        store.clone(),
+        execution_state.clone(),
+        tx_batch_loader,
+    )
+    .await
+    .unwrap();
+    Subscriber::spawn(subscriber);
+
+    (store, execution_state)
+}
 
 #[tokio::test]
 async fn handle_consensus_message() {
@@ -26,22 +55,7 @@ async fn handle_consensus_message() {
     tokio::task::yield_now().await;
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store,
-        execution_state,
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let _ = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Feed certificates to the mock sequencer and ensure the batch loader receive the command to
     // download the corresponding transaction data.
@@ -70,22 +84,7 @@ async fn synchronize() {
     }
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store,
-        execution_state,
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let _ = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Send two extra certificates. The client needs to sync for the first two certificates.
     for _ in 0..2 {
@@ -110,22 +109,7 @@ async fn execute_transactions() {
     tokio::task::yield_now().await;
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store.clone(),
-        execution_state.clone(),
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let (store, execution_state) = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Feed certificates to the mock sequencer and add the transaction data to storage (as if
     // the batch loader downloaded them).
@@ -168,22 +152,7 @@ async fn execute_empty_certificate() {
     tokio::task::yield_now().await;
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store.clone(),
-        execution_state.clone(),
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let (store, execution_state) = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Feed empty certificates to the mock sequencer.
     for _ in 0..2 {
@@ -218,22 +187,7 @@ async fn execute_malformed_transactions() {
     tokio::task::yield_now().await;
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store.clone(),
-        execution_state.clone(),
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let (store, execution_state) = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Feed a bad transaction to the mock sequencer
     let tx0 = MALFORMED_TRANSACTION;
@@ -288,22 +242,7 @@ async fn internal_error_execution() {
     tokio::task::yield_now().await;
 
     // Spawn a subscriber.
-    let store_path = tempfile::tempdir().unwrap();
-    const BATCHES_CF: &str = "batches";
-    let rocksdb = open_cf(store_path, None, &[BATCHES_CF]).unwrap();
-    let batch_map = reopen!(&rocksdb, BATCHES_CF;<BatchDigest, SerializedBatchMessage>);
-    let store = Store::new(batch_map);
-
-    let execution_state = Arc::new(TestExecutionState::default());
-    let subscriber = Subscriber::<TestExecutionState, Ed25519PublicKey>::new(
-        node_address,
-        store.clone(),
-        execution_state.clone(),
-        tx_batch_loader,
-    )
-    .await
-    .unwrap();
-    Subscriber::spawn(subscriber);
+    let (store, execution_state) = spawn_subscriber(node_address, tx_batch_loader).await;
 
     // Feed a killer transaction to the mock sequencer
     let tx0 = KILLER_TRANSACTION;
