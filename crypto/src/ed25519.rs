MystenLabs/narwhal

commit fe7c11c8ef848a2e1c9a88e2002192484958a5f0

refactor: replace the waiter's cancellation channel by a oneshot::channel

primary/src/certificate_waiter.rs
@@ -18,8 +18,8 @@ use std::{
 use store::Store;
 use tokio::{
     sync::{
-        mpsc::{channel, Receiver, Sender},
-        watch,
+        mpsc::{Receiver, Sender},
+        oneshot, watch,
     },
     task::JoinHandle,
 };
@@ -49,7 +49,7 @@ pub struct CertificateWaiter<PublicKey: VerifyingKey> {
     /// List of digests (certificates) that are waiting to be processed. Their processing will
     /// resume when we get all their dependencies. The map holds a cancellation `Sender`
     /// which we can use to give up on a certificate.
-    pending: HashMap<HeaderDigest, (Round, Sender<()>)>,
+    pending: HashMap<HeaderDigest, (Round, oneshot::Sender<()>)>,
     /// The metrics handler
     metrics: Arc<PrimaryMetrics>,
 }
@@ -88,7 +88,7 @@ impl<PublicKey: VerifyingKey> CertificateWaiter<PublicKey> {
         missing: Vec<CertificateDigest>,
         store: &Store<CertificateDigest, Certificate<PublicKey>>,
         deliver: Certificate<PublicKey>,
-        mut cancel_handle: Receiver<()>,
+        cancel_handle: oneshot::Receiver<()>,
     ) -> DagResult<Certificate<PublicKey>> {
         let waiting: Vec<_> = missing.into_iter().map(|x| store.notify_read(x)).collect();
 
@@ -97,7 +97,7 @@ impl<PublicKey: VerifyingKey> CertificateWaiter<PublicKey> {
                 result.map(|_| deliver).map_err(DagError::from)
             }
             // the request for this certificate is obsolete, for instance because its round is obsolete (GC'd).
-            _ = cancel_handle.recv() => Ok(deliver),
+            _ = cancel_handle => Ok(deliver),
         }
     }
 
@@ -124,7 +124,7 @@ impl<PublicKey: VerifyingKey> CertificateWaiter<PublicKey> {
                         .parents
                         .iter().cloned()
                         .collect();
-                    let (tx_cancel, rx_cancel) = channel(1);
+                    let (tx_cancel, rx_cancel) = oneshot::channel();
                     self.pending.insert(header_id, (certificate.round(), tx_cancel));
                     let fut = Self::waiter(wait_for, &self.store, certificate, rx_cancel);
                     waiting.push(fut);
@@ -160,12 +160,20 @@ impl<PublicKey: VerifyingKey> CertificateWaiter<PublicKey> {
             let round = self.consensus_round.load(Ordering::Relaxed);
             if round > self.gc_depth {
                 let gc_round = round - self.gc_depth;
-                for (r, cancel_handle) in self.pending.values() {
-                    if *r <= gc_round + 1 {
-                        let _ = cancel_handle.send(()).await;
-                    }
-                }
-                self.pending.retain(|_, (r, _)| *r > gc_round + 1);
+                self.pending = self
+                    .pending
+                    .drain()
+                    .flat_map(|(digest, (r, handler))| {
+                        if r <= gc_round {
+                            handler
+                                .send(())
+                                .expect("fatal error sending pending cancellation signal");
+                            None
+                        } else {
+                            Some((digest, (r, handler)))
+                        }
+                    })
+                    .collect();
             }
 
             self.update_metrics();

primary/src/header_waiter.rs
@@ -24,8 +24,8 @@ use std::{
 use store::Store;
 use tokio::{
     sync::{
-        mpsc::{channel, Receiver, Sender},
-        watch,
+        mpsc::{Receiver, Sender},
+        oneshot, watch,
     },
     task::JoinHandle,
     time::{sleep, Duration, Instant},
@@ -88,7 +88,7 @@ pub struct HeaderWaiter<PublicKey: VerifyingKey> {
     batch_requests: HashMap<BatchDigest, Round>,
     /// List of digests (headers or tx batch) that are waiting to be processed.
     /// Their processing will resume when we get all their dependencies.
-    pending: HashMap<HeaderDigest, (Round, Sender<()>)>,
+    pending: HashMap<HeaderDigest, (Round, oneshot::Sender<()>)>,
     /// Metrics handler
     metrics: Arc<PrimaryMetrics>,
 }
@@ -148,7 +148,7 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
         missing: Vec<T>,
         store: Store<T, V>,
         deliver: Header<PublicKey>,
-        mut handler: Receiver<()>,
+        handler: oneshot::Receiver<()>,
     ) -> DagResult<Option<Header<PublicKey>>>
     where
         T: Serialize + DeserializeOwned + Send + Clone,
@@ -159,7 +159,7 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
             result = try_join_all(waiting) => {
                 result.map(|_| Some(deliver)).map_err(DagError::from)
             }
-            _ = handler.recv() => Ok(None),
+            _ = handler => Ok(None),
         }
     }
 
@@ -190,7 +190,7 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
                             let wait_for = missing
                                 .iter().map(|(x, y)| (*x, *y))
                                 .collect();
-                            let (tx_cancel, rx_cancel) = channel(1);
+                            let (tx_cancel, rx_cancel) = oneshot::channel();
                             self.pending.insert(header_id, (round, tx_cancel));
                             let fut = Self::waiter(wait_for, self.payload_store.clone(), header, rx_cancel);
                             // pointer-size allocation, bounded by the # of blocks (may eventually go away, see rust RFC #1909)
@@ -231,7 +231,7 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
                             // Add the header to the waiter pool. The waiter will return it to us
                             // when all its parents are in the store.
                             let wait_for = missing.clone();
-                            let (tx_cancel, rx_cancel) = channel(1);
+                            let (tx_cancel, rx_cancel) = oneshot::channel();
                             self.pending.insert(header_id, (round, tx_cancel));
                             let fut = Self::waiter(wait_for, self.certificate_store.clone(), header, rx_cancel);
                             // pointer-size allocation, bounded by the # of blocks (may eventually go away, see rust RFC #1909)
@@ -337,12 +337,22 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
 
                 let mut gc_round = round - self.gc_depth;
 
-                for (r, handler) in self.pending.values() {
-                    if r <= &gc_round {
-                        let _ = handler.send(()).await;
-                    }
-                }
-                self.pending.retain(|_, (r, _)| r > &mut gc_round);
+                // Cancel expired `notify_read`s, keep the rest in the map
+                // TODO: replace with `drain_filter` once that API stabilizes
+                self.pending = self
+                    .pending
+                    .drain()
+                    .flat_map(|(digest, (r, handler))| {
+                        if r <= gc_round {
+                            handler
+                                .send(())
+                                .expect("fatal error sending pending cancellation signal");
+                            None
+                        } else {
+                            Some((digest, (r, handler)))
+                        }
+                    })
+                    .collect();
                 self.batch_requests.retain(|_, r| r > &mut gc_round);
                 self.parent_requests.retain(|_, (r, _)| r > &mut gc_round);
 

commit 247d9fc2121b31c5bd6cacf28094a618a540c13e

fix: do not panic on send failure for `waiter` items
+ adds comment on reason for failure

The panic was introduced in fe7c11c8ef848a2e1c9a88e2002192484958a5f0, and its scope reduced in
d9b0f049938ec8cea11a0afe93db381efc4c67d5, but really should not have been there in the first place.

crypto/src/ed25519.rs
@@ -5,14 +5,18 @@ use base64ct::{Base64, Encoding};
 use serde::{de, Deserialize, Serialize};
 use serde_with::serde_as;
 use signature::{Signature, Signer, Verifier};
-use std::fmt::{self, Display};
-use std::str::FromStr;
+use std::{
+    fmt::{self, Display},
+    str::FromStr,
+};
 
-use crate::pubkey_bytes::PublicKeyBytes;
-use crate::serde_helpers::Ed25519Signature as Ed25519Sig;
-use crate::traits::{
-    AggregateAuthenticator, Authenticator, EncodeDecodeBase64, KeyPair, SigningKey, ToFromBytes,
-    VerifyingKey,
+use crate::{
+    pubkey_bytes::PublicKeyBytes,
+    serde_helpers::Ed25519Signature as Ed25519Sig,
+    traits::{
+        AggregateAuthenticator, Authenticator, EncodeDecodeBase64, KeyPair, SigningKey,
+        ToFromBytes, VerifyingKey,
+    },
 };
 
 ///

crypto/src/serde_helpers.rs
@@ -3,10 +3,11 @@
 
 use base64ct::Encoding as _;
 use blst::min_sig as blst;
-use serde::de::{Deserializer, Error};
-use serde::ser::Serializer;
-use serde::Deserialize;
-use serde::Serialize;
+use serde::{
+    de::{Deserializer, Error},
+    ser::Serializer,
+    Deserialize, Serialize,
+};
 use serde_with::{Bytes, DeserializeAs, SerializeAs};
 use std::fmt::Debug;
 

primary/src/certificate_waiter.rs
@@ -187,11 +187,12 @@ impl<PublicKey: VerifyingKey> CertificateWaiter<PublicKey> {
 
                 self.pending.retain(|_digest, (r, once_cancel)| {
                     if *r <= gc_round {
-                        once_cancel
+                        // note: this send can fail, harmlessly, if the certificate has been delivered (`notify_read`)
+                        // and the present code path fires before the corresponding `waiting` item is unpacked above.
+                        let _ = once_cancel
                             .take()
                             .expect("This should be protected by a write lock")
-                            .send(())
-                            .expect("fatal error sending pending cancellation signal");
+                            .send(());
                         false
                     } else {
                         true

primary/src/header_waiter.rs
@@ -344,9 +344,9 @@ impl<PublicKey: VerifyingKey> HeaderWaiter<PublicKey> {
                     .drain()
                     .flat_map(|(digest, (r, handler))| {
                         if r <= gc_round {
-                            handler
-                                .send(())
-                                .expect("fatal error sending pending cancellation signal");
+                            // note: this send can fail, harmlessly, if the certificate has been delivered (`notify_read`)
+                            // and the present code path fires before the corresponding `waiting` item is unpacked above.
+                            let _ = handler.send(());
                             None
                         } else {
                             Some((digest, (r, handler)))