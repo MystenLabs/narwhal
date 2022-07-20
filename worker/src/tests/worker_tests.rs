9c7df8e3185b43c8b7fc8cc174c14689f78b8809
Alberto Sonnino <alberto@sonnino.com>
2022-07-20 16:30:59 -0400

    fix worker tests

--- worker/src/tests/worker_tests.rs
+++ worker/src/tests/worker_tests.rs
@@ -4,7 +4,7 @@
 use super::*;
 
 use arc_swap::ArcSwap;
-use crypto::traits::KeyPair;
+use crypto::{ed25519::Ed25519PublicKey, traits::KeyPair};
 use futures::StreamExt;
 use primary::WorkerPrimaryMessage;
 use prometheus::Registry;
@@ -54,7 +54,11 @@ async fn handle_clients_transactions() {
     let batch_digest = serialized_batch_digest(&serialized_batch).unwrap();
 
     let primary_address = committee.primary(&name).unwrap().worker_to_primary;
-    let expected = bincode::serialize(&WorkerPrimaryMessage::OurBatch(batch_digest, id)).unwrap();
+    let expected = bincode::serialize(&WorkerPrimaryMessage::<Ed25519PublicKey>::OurBatch(
+        batch_digest,
+        id,
+    ))
+    .unwrap();
     let mut handle = WorkerToPrimaryMockServer::spawn(primary_address);
 
     // Spawn enough workers' listeners to acknowledge our batches.
