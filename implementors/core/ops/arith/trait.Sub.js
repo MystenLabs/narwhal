b94d8411de28757a036cbe2ab08ccf8d477fc188
punwai <punwai@users.noreply.github.com>
2022-08-15 23:14:20 +0000

    deploy: 627f65f22cbe32dd7eb7d746758a7a3a64bb1afd

---
+++ implementors/core/ops/arith/trait.Sub.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"crypto":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"crypto/bulletproofs/struct.PedersenCommitment.html\" title=\"struct crypto::bulletproofs::PedersenCommitment\">PedersenCommitment</a>&gt; for <a class=\"struct\" href=\"crypto/bulletproofs/struct.PedersenCommitment.html\" title=\"struct crypto::bulletproofs::PedersenCommitment\">PedersenCommitment</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
