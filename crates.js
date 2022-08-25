5604ce29065bc9a9870848994f3fe5d3fc751334
huitseeker <huitseeker@users.noreply.github.com>
2022-08-25 22:55:40 +0000

    deploy: c59d2024e6a515908369b72f99a5704495a7b718

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["config","consensus","crypto","dag","demo_client","executor","network","node","primary","test_utils","types","worker","workspace_hack"];
+window.ALL_CRATES = ["config","consensus","crypto","dag","demo_client","executor","network","node","primary","storage","test_utils","types","worker","workspace_hack"];
