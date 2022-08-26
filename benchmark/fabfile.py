15a699f4e91151fb68eb3717242d75d33f9425a6
sh1hsh1nk <shashank.motepalli@mail.utoronto.ca>
2022-08-26 15:25:47 -0400

    tx rate is too low, min is 20

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -178,7 +178,7 @@ def remote(ctx, debug=False):
         'nodes': [4],
         'workers': 1,
         'collocate': True,
-        'rate': [1],
+        'rate': [20],
         'tx_size': 32,
         'duration': 300,
         'runs': 10,
