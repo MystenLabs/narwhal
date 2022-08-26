16e67a16a3ac6871ac65c85684eee0c4fe76fc0e
sh1hsh1nk <shashank.motepalli@mail.utoronto.ca>
2022-08-26 15:18:37 -0400

    32 byte messages

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -178,21 +178,20 @@ def remote(ctx, debug=False):
         'nodes': [4],
         'workers': 1,
         'collocate': True,
-        'rate': [50_000],
-        'tx_size': 512,
+        'rate': [1],
+        'tx_size': 32,
         'duration': 300,
-        'runs': 2,
+        'runs': 10,
         'mem_profiling': False
     }
     node_params = {
-        'header_size': 1_000,  # bytes
-        'max_header_delay': '200ms',  # ms
+        'header_size': 4,  # bytes
+        'max_header_delay': '20ms',  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': '10_000ms',  # ms
         'sync_retry_nodes': 3,  # number of nodes
-        'batch_size': 500_000,  # bytes
-        'max_batch_delay': '200ms',  # ms,
-	'max_batch_delay': '200ms',  #ms
+        'batch_size': 32,  # bytes
+        'max_batch_delay': '20ms',  # ms,
         'block_synchronizer': {
             'certificates_synchronize_timeout': '2_000ms',
             'payload_synchronize_timeout': '2_000ms',
