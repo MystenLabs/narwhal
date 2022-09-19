b8b33b73b0545a58fd96c406a12a2e2b06bcf80b
Evan Chan <velvia@gmail.com>
2022-09-19 11:27:44 -0700

    Add code to raise ulimit automatically in tests (#1006)

--- test_utils/src/lib.rs
+++ test_utils/src/lib.rs
@@ -46,6 +46,14 @@ pub fn temp_dir() -> std::path::PathBuf {
         .into_path()
 }
 
+pub fn ensure_test_environment() {
+    // One common issue when running tests on Mac is that the default ulimit is too low,
+    // leading to I/O errors such as "Too many open files". Raising fdlimit to bypass it.
+    // Also we can't do this in Windows, apparently.
+    #[cfg(not(target_os = "windows"))]
+    fdlimit::raise_fd_limit().expect("Could not raise ulimit");
+}
+
 #[macro_export]
 macro_rules! test_channel {
     ($e:expr) => {
