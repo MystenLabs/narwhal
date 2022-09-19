8b0cf9ada5f8c34efcd253e8af4c05490212f240
mwtian <mwtian@users.noreply.github.com>
2022-09-19 18:49:32 +0000

    deploy: f74daebb1e70c6560471202d10d805eecba2cdfc

--- implementors/executor/errors/trait.ExecutionStateError.js
+++ implementors/executor/errors/trait.ExecutionStateError.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"node":[["impl ExecutionStateError for <a class=\"enum\" href=\"node/execution_state/enum.SimpleExecutionError.html\" title=\"enum node::execution_state::SimpleExecutionError\">SimpleExecutionError</a>"]]
+"node":[["impl <a class=\"trait\" href=\"executor/errors/trait.ExecutionStateError.html\" title=\"trait executor::errors::ExecutionStateError\">ExecutionStateError</a> for <a class=\"enum\" href=\"node/execution_state/enum.SimpleExecutionError.html\" title=\"enum node::execution_state::SimpleExecutionError\">SimpleExecutionError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
