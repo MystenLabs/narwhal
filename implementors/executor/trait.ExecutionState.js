658490282ad11dddb60649af2389e9cc07701864
mwtian <mwtian@users.noreply.github.com>
2022-09-20 00:44:19 +0000

    deploy: be06fa79a2971cc6311d055332eb940d0f4651b6

--- implementors/executor/trait.ExecutionState.js
+++ implementors/executor/trait.ExecutionState.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"node":[["impl ExecutionState for <a class=\"struct\" href=\"node/execution_state/struct.SimpleExecutionState.html\" title=\"struct node::execution_state::SimpleExecutionState\">SimpleExecutionState</a>"]]
+"node":[["impl <a class=\"trait\" href=\"executor/trait.ExecutionState.html\" title=\"trait executor::ExecutionState\">ExecutionState</a> for <a class=\"struct\" href=\"node/execution_state/struct.SimpleExecutionState.html\" title=\"struct node::execution_state::SimpleExecutionState\">SimpleExecutionState</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
