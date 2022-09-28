448dcf5f898f6bb6bf4841369076bc99395937e7
akichidis <akichidis@users.noreply.github.com>
2022-09-28 20:55:10 +0000

    deploy: 46bb2d73c17b2fd6b11fb08b73808dcb56434ad0

--- implementors/narwhal_executor/errors/trait.ExecutionStateError.js
+++ implementors/narwhal_executor/errors/trait.ExecutionStateError.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"narwhal_node":[["impl ExecutionStateError for <a class=\"enum\" href=\"narwhal_node/execution_state/enum.SimpleExecutionError.html\" title=\"enum narwhal_node::execution_state::SimpleExecutionError\">SimpleExecutionError</a>"]]
+"narwhal_node":[["impl <a class=\"trait\" href=\"narwhal_executor/errors/trait.ExecutionStateError.html\" title=\"trait narwhal_executor::errors::ExecutionStateError\">ExecutionStateError</a> for <a class=\"enum\" href=\"narwhal_node/execution_state/enum.SimpleExecutionError.html\" title=\"enum narwhal_node::execution_state::SimpleExecutionError\">SimpleExecutionError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
